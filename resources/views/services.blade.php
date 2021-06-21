4c90bbb50c46332d0ea97c766b5d6d1f64cb99f6
Galyna <g.sukhovskaya@gmail.com>
2021-06-21 23:45:06 +0300

    dz 08.06.2021 Создана ветка в git "dev"

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -3,23 +3,24 @@
 @section('title', 'SERVICES')
 
 @section('content_1')
-                    <div class="axil-post-list-area axil-section-gap bg-color-white">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-lg-8 col-xl-8">
-                                    <!-- Start About Area  -->
-                                    <div class="axil-about-us">
-                                        <div class="inner">
-                                            <ul>
-                                                <li>We may provide You the best services</li>
-                                                <li>We understand the varying needs of each individual market place and always strive to provide you the best possible solution both from a technical and economic perspective.</li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <!-- End About Area  -->
-                                </div>
-
-
+    <div class="axil-post-list-area axil-section-gap bg-color-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-xl-8">
+                    <!-- Start About Area  -->
+                    <div class="axil-about-us">
+                        <div class="inner">
+                            <ul>
+                                <li>We may provide You the best services</li>
+                                <li>We understand the varying needs of each individual market place and always strive to
+                                    provide you the best possible solution both from a technical and economic
+                                    perspective.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- End About Area  -->
+                </div>
 @endsection
 
 @section('side_bar')
--- routes/api.php
+++ routes/api.php
@@ -18,14 +18,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts', function (){
+Route::get('/posts', function () {
     return response()->json(\App\Post::paginate(10), 200);
 });
 
-Route::get('/post/{id}', function ($id){
+Route::get('/post/{id}', function ($id) {
     try {
         $post = \App\Post::findOrFail($id);
-    } catch (Exception $exception){
+    } catch (Exception $exception) {
         return response()->json(['Message' => "Such post haven't been found"], 404);
     }
     return response()->json($post, 200);
